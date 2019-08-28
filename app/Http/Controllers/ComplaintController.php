a4ecce14cdad50637471476cf063decf1b341215
juniarsofyan <choerulsofyanmf@gmail.com>
2019-08-28 16:00:15 +0700

    Complaint Module - CRD operation for Complaint module

---
+++ app/Http/Controllers/ComplaintController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Complaint;
+
+class ComplaintController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $complaints = Complaint::orderBy('created_at', 'DESC')->get();
+        return response()->json($complaints);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|string',
+            'subject' => 'required|string',
+            'description' => 'required|string'
+        ]);
+
+        $complaint = Complaint::firstOrCreate([
+            'email' => $request->email,
+            'subject' => $request->subject,
+            'description' => $request->description
+        ]);
+
+        return response()->json($complaint, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $complaint = Complaint::findOrFail($id);
+        return response()->json($complaint);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Complaint::findOrFail($id)->delete();
+        return response()->json('Complaint deleted successfully');
+    }
+}
